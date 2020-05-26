fa8ef4828b980c16495f2bb9f01be7e470cb0133
Jenni <jenni.svensson@live.se>
2020-05-26 14:18:32 +0200

    finished the server section

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const axios = require("axios");
+
+const port = 4000;
+
+const app = express();
+
+function onListen() {
+  console.log(`Listening on :${port}`);
+}
+
+async function onRequestPatients(request, response) {
+  const patients = await axios.get(
+    "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/patients"
+  );
+
+  const data = patients.data;
+  response.send(JSON.stringify(data));
+}
+
+async function onRequestDoctors(request, response) {
+  const doctors = await axios.get(
+    "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/doctors"
+  );
+  // console.log(patients.data);
+  //const document = render(patients);
+  const data = doctors.data;
+
+  //is this the correct output?? it says array in the rubric
+
+  response.send(JSON.stringify(data));
+}
+
+async function onRequestId(request, response) {
+  const params = request.params.id;
+  const patient = await axios.get(
+    `https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/patients/${params}`
+  );
+  const data = patient.data;
+  const id = request.params.id;
+
+  response.send(JSON.stringify(data, null, 2));
+}
+
+app.get("/patients", onRequestPatients);
+app.get("/doctors", onRequestDoctors);
+app.get("/patients/:id", onRequestId);
+app.listen(port, onListen);
